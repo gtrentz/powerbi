bec78f467e2a88c35ee55e957320f93724cdc6de
gtrentz <grant-trentz@uiowa.edu>
2023-05-15 23:01:02 -0500

    ...

---
+++ src/GraphGUI.java
@@ -0,0 +1,16 @@
+/**
+ * Created: 05/15/2023
+ * @author gtrentz
+ */
+package src;
+import java.awt.*;
+
+public class GraphGUI {
+    Graphics g = new Graphics();
+    private void startScreen() {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
